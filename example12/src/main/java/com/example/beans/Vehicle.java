820211f4e513cdac89314b5a4e7defccec2f1e96
nikhil <kumarnikil220@gmail.com>
2024-07-14 20:45:01 +0530

    Injecting beans using @Autowired on class fields

---
+++ example12/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,27 @@
+package com.example.beans;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Vehicle {
+
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String hello(){
+        return "Hello from Vehicle Pojo class";
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
