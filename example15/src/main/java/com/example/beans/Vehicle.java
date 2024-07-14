a67237a626b1f30a8c0a17e73273885446e67fa5
nikhil <kumarnikil220@gmail.com>
2024-07-14 22:59:36 +0530

    types of Autowiring and @Qualifier annotation

---
+++ example15/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,29 @@
+package com.example.beans;
+import org.springframework.stereotype.Component;
+
+public class Vehicle {
+
+    public Vehicle(){
+        System.out.println("Vehicle bean created by Spring");
+    }
+
+    private String name="Toyota";
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String hello(){
+        return "Hello from Vehicle Pojo class";
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
